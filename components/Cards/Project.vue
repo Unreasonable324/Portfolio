348da4dd81b10be4633ca5254f8bc976ba523e56
John Doe <johndoe@email.com>
2024-07-09 12:55:08 +0700

    fix

--- components/Cards/Project.vue
+++ components/Cards/Project.vue
@@ -29,10 +29,10 @@ const clickImage = (img: string) => {
         :showIndicators="true"
         showIndicatorsOnItem
         v-model:activeIndex="activeIndex"
-        :pt="{ root: '!border-none border-[#0F1624]' }"
+        :pt="{ root: '!border-none border-[#0F1624] ' , }"
       >
         <template #item="slotProps">
-          <PrimeImage alt="Image" :src="slotProps.item" class="h-[300px] cursor-pointer" @click="clickImage(slotProps.item)">
+          <PrimeImage alt="Image" :src="slotProps.item" class="h-[300px] cursor-pointer flex items-center justify-center"  @click="clickImage(slotProps.item)">
             <template #image>
               <NuxtImg alt="Image" :src="slotProps.item" class="w-[100%] object-scale-down" style="width: 100%; display: block"></NuxtImg>
             </template>
@@ -65,7 +65,7 @@ const clickImage = (img: string) => {
         :showItemNavigators="true"
         :showThumbnails="false"
         containerStyle="width: 100vw; height: calc(100vh ); border: none"
-        :pt="{ mask: '!z-[1200]', item: 'h-[calc(100vh)]', items: 'items-center', closeButton: '!absolute right-5 top-5 z-[1200]' }"
+        :pt="{ mask: '!z-[1200]', item: 'h-[calc(100vh)]', items: '!items-center', closeButton: '!absolute right-5 top-5 z-[1200]' }"
       >
         <template #item="slotProps">
           <NuxtImg alt="Image" :src="slotProps.item" class="object-scale-down" style="width: 90%; height: 80%; display: block"></NuxtImg>
